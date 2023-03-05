9f7c94f51b43ff2d694f35b3449866f4cc0628a1
cariocaphil <cariocaphil@gmail.com>
2023-03-05 19:25:06 +0100

    add personal info

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I'm a software engineer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        Originally from a media background, I fell in love with front-end development while working for a web agency in 2015. I have been enjoying the pleasures of tackling technical challenges, problem-solving and exploring the latest web technologies ever since. Recently, I obtained the Bachelor's-level diploma in Front-End-Development from <a href="https://openclassrooms.com/en/paths/315-front-end-developer">Openclassrooms</a>. Ideally, my work is at the service of a higher, frictionless user experience. In my freetime I'm a sports-geek (Basketball), an <a href="https://www.goodreads.com/user/show/129723443-cariocaphil"> avid reader </a>  and a restless traveller.        </p>
       </section>
     </Layout>
   )
