602f2b3982bfc5123c40fdda6f6d14718576096a
cariocaphil <cariocaphil@gmail.com>
2023-03-05 22:08:36 +0100

    add rendering of markdown content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,13 +3,14 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+    const postData = await getPostData(params.id);
+  
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -21,12 +22,14 @@ export async function getStaticPaths() {
 
 export default function Post({ postData }) {
     return (
-      <Layout>
+        <Layout>
         {postData.title}
         <br />
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     );
   }
