2e0fd1b154a19e56788815321933f7f4d95a6f41
cariocaphil <cariocaphil@gmail.com>
2023-03-05 19:49:06 +0100

    add fetching data (from file system), pre-render index page with it

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,6 +23,20 @@ export default function Home() {
         <p>I'm a software engineer</p>
         <p>
         Originally from a media background, I fell in love with front-end development while working for a web agency in 2015. I have been enjoying the pleasures of tackling technical challenges, problem-solving and exploring the latest web technologies ever since. Recently, I obtained the Bachelor's-level diploma in Front-End-Development from <a href="https://openclassrooms.com/en/paths/315-front-end-developer">Openclassrooms</a>. Ideally, my work is at the service of a higher, frictionless user experience. In my freetime I'm a sports-geek (Basketball), an <a href="https://www.goodreads.com/user/show/129723443-cariocaphil"> avid reader </a>  and a restless traveller.        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
